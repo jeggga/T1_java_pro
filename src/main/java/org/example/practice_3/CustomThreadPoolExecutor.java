1a15cd65f536559201119bd72b49ed8197855995
Serkov_EY <ESerkov@inno.local>
2024-10-02 16:36:02 +0300

    feature/pravtice-3: refactoring after review

--- src/main/java/org/example/practice_3/CustomThreadPoolExecutor.java
+++ src/main/java/org/example/practice_3/CustomThreadPoolExecutor.java
@@ -3,14 +3,14 @@ package org.example.practice_3;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CustomThreadPoolExecutor implements Executor {
     private static final System.Logger LOG = System.getLogger("CustomThreadPoolExecutor");
 
     private final LinkedList<Runnable> threadsQueue = new LinkedList<>();
 
-    private Boolean isActive = true;
-    private Boolean isAwaitTermination = false;
+    private AtomicBoolean isActive = new AtomicBoolean(true);
 
     public CustomThreadPoolExecutor(Integer capacity) {
         for (int i = 0; i < capacity; i++) {
@@ -20,7 +20,7 @@ public class CustomThreadPoolExecutor implements Executor {
 
     @Override
     public void execute(Runnable command) {
-        if (isActive) {
+        if (isActive.get()) {
             threadsQueue.addLast(command);
         } else {
             throw new IllegalStateException("Threads are shutdown");
@@ -28,11 +28,11 @@ public class CustomThreadPoolExecutor implements Executor {
     }
 
     public void shutdown() {
-        isActive = false;
+        isActive.set(false);
     }
 
     public void awaitTermination() {
-        isAwaitTermination = true;
+
     }
 
     private class Worker implements Runnable {
@@ -40,29 +40,15 @@ public class CustomThreadPoolExecutor implements Executor {
         @Override
         public void run() {
             LOG.log(System.Logger.Level.INFO, String.format("%s is started", Thread.currentThread().getName()));
-            mainWorker();
-            afterInterruptWorker();
-            LOG.log(System.Logger.Level.INFO, String.format("%s is interrupt", Thread.currentThread().getName()));
-        }
-
-        private void mainWorker() {
-            while (isActive) {
+            while (isActive.get()) {
                 try {
                     getNextTask().run();
                 } catch (NoSuchElementException e) {
                     LOG.log(System.Logger.Level.INFO, String.format("%s waiting new task", Thread.currentThread().getName()));
                 }
-            }
-        }
 
-        private void afterInterruptWorker() {
-            while (isAwaitTermination) {
-                try {
-                    getNextTask().run();
-                } catch (NoSuchElementException e) {
-                    break;
-                }
             }
+            LOG.log(System.Logger.Level.INFO, String.format("%s is interrupt", Thread.currentThread().getName()));
         }
     }
 
